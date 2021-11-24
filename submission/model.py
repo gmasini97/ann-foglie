ba1f997ccbbe0c99508571181317ea2aa36f6b92
gmasini97 <20663565+gmasini97@users.noreply.github.com>
2021-11-24 23:12:58 +0100

    transfer healthy/unealthy

--- submission/model.py
+++ submission/model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class model:
     def __init__(self, path):
-        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel/xception-fine'))
+        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel/xception-full-fine'))
 
     def predict(self, X):
         out = self.model.predict(X)
