3fa21e05a6b1e980fb298a0de0d9249dc6dc543d
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2021-11-24 23:11:42 +0100

    Update configs.py

--- utilities/configs.py
+++ utilities/configs.py
@@ -1,5 +1,6 @@
 dataset_dir = './dataset/training'
 labels = ['Apple','Blueberry','Cherry','Corn','Grape','Orange','Peach','Pepper','Potato','Raspberry','Soybean','Squash','Strawberry','Tomato']
+labels_sick = ['Apple','Apple_sick','Blueberry','Cherry','Corn','Grape','Orange','Peach','Pepper','Potato','Raspberry','Soybean','Squash','Strawberry','Tomato']
 SEED = 19011997
 SPLIT = 0.1
 BATCH_SIZE = 128
