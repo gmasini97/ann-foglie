54745e4de2e0ce3be5a0978050461a7eead36bdb
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2021-11-27 19:48:25 +0100

    VGG from scratch

--- submission/model.py
+++ submission/model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class model:
     def __init__(self, path):
-        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel/xception-full-fine'))
+        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel/transfer_2_dense_fine'))
 
     def predict(self, X):
         out = self.model.predict(X)
